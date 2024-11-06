a78f10c5c6d2c58909d0a85b4b4a9fb7a9aae4cf
migue244 <00001105806200sp@al.educacao.sp.gov.br>
2024-11-06 09:23:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'quem tem mais titulos da champions league?',
+    'real madrid com 15 conquistas'
+)
+
+criaCartao(
+    'futebol',
+    'quantas copas do mundo o brasil tem?',
+    '5 campeonatos mundiais'
+)
+
+criaCartao(
+    'futebol',
+    'quem possui o maior numeros de titulos da libertadores?',
+    'independente(arg) com 7 titulos'
+)
+
+criaCartao(
+    'futebol',
+    'quem é o maior artilheiro do mundo de todos os tempos?',
+    'cristiano ronaldo com 907(até o momento)'
+)
